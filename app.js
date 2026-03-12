6f7004773475f5a7ee12e66409c274fc666cc676
ayuda-inmediata-japon <joseantonio.garciau@gmail.com>
2026-03-13 07:06:02 +0900

    Apoyar,guardar app perfecta

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-1document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("searchInput");
   const searchBtn = document.getElementById("searchBtn");
   

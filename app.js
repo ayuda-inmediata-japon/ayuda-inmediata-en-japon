15a8089622f0b0353e1424618a64ee80c7c75bfb
ayuda-inmediata-japon <joseantonio.garciau@gmail.com>
2026-03-13 20:55:38 +0900

    Correct typo in document event listener
    Fix typo in 'document' variable name.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+documen.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("searchInput");
   const searchBtn = document.getElementById("searchBtn");
   

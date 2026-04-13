5d66f14a349e195ad70abfcd89708d2b8c77e70c
ayuda-inmediata-japon <joseantonio.garciau@gmail.com>
2026-04-13 16:57:22 +0900

    Actualizar app.js

--- app.js
+++ app.js
@@ -335,7 +335,7 @@ style="flex:1;background:#25D366;color:white;padding:10px;border:none;border-rad
 ⬅ Volver al buscador
 </button>
 
-<button onclick="document.getElementById('Llegada a Japón  ').scrollIntoView({behavior:'smooth'})"
+<button onclick="document.getElementById('llegada a japon ').scrollIntoView({behavior:'smooth'})"
 style="flex:1;background:#25D366;color:white;padding:10px;border:none;border-radius:8px;">
 ⬆ Volver al tema
 </button>

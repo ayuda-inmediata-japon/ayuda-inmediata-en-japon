1e856927f9eb29fbb4fcf0559b3eba35433aa6af
ayuda-inmediata-japon <joseantonio.garciau@gmail.com>
2026-04-26 19:17:07 +0900

    Actualizar app.js

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ document.addEventListener("DOMContentLoaded", () => {
 <div class="subtema-content">
 <p><strong>Qué hacer:</strong></p>
 <ol>
-<img src="Img/llegada.jpg" alt="Llegada a Japón">
+<img src="Img/llegada.jpg"
+style="width:calc(100% - 56px); max-width:540px; display:block; margin:10px auto 18px auto; border-radius:18px;"
+alt="Llegada a Japón">
 <li>Al bajar del avión sigue los letreros de <strong>Arrivals / 到着</strong>.</li>
 <li>No te apresures; normalmente el camino está bien indicado.</li>
 <li>Ten a mano tu pasaporte y la dirección de tu alojamiento.</li>

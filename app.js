22b36734d1518717849bcb5325c83c104fbb2aed
Ayuda Inmediata en Japón <joseantonio.garciau@gmail.com>
2026-05-02 08:00:01 +0900

    Update disaster images in app.js

--- app.js
+++ app.js
@@ -791,7 +791,7 @@ emergencia: `
 <summary>🔥 Incendio o rescate</summary>
 <div class="subtema-content">
 
-<img src="TU_IMAGEN_BOMBEROS" class="info-img" alt="Bomberos en Japón">
+<img src= "incendio en japon.jpg" class="info-img" alt="Bomberos en Japón">
 
 <p><strong>Qué hacer:</strong></p>
 <ol>
@@ -814,7 +814,7 @@ emergencia: `
 <summary>🌏 Terremoto o tsunami</summary>
 <div class="subtema-content">
 
-<img src="TU_IMAGEN_DESASTRES" class="info-img" alt="Desastres en Japón">
+<img src="terremoto en japon.jpg" class="info-img" alt="Desastres en Japón">
 
 <p><strong>Terremoto:</strong></p>
 <ul>
